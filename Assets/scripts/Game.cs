87c753cc8bb6629a98ae9468d4e3e371262f4f92
Snass <thibaut.dimartino@epitech.eu>
2020-12-03 10:51:48 +0100

    game save

---
+++ Assets/scripts/Game.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public void SaveGame()
+    {
+        Save save = CreateSaveGameObject();
+
+        // Creating the file where the game will be saved
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+        Debug.Log("Game Saved");
+    }
+    public void LoadGame()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            Save save = (Save)bf.Deserialize(file);
+            file.Close();
+            Debug.Log("Game Loaded");
+
+            Unpause();
+        }
+        else
+        {
+            Debug.Log("No game saved!");
+        }
+    }
+
+
+    public void SaveAsJSON()
+    {
+        Save save = CreateSaveGameObject();
+        string json = JsonUtility.ToJson(save);
+
+        Debug.Log("Saving as JSON: " + json);
+    }
+
+    private Save CreateSaveGameObject()
+    {
+        Save save = new Save();
+        return save;
+    }
+}
