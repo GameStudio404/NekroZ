54396164ad2827b59daa684d7eb52723a30292ce
Thomas Ducret <ducret.thomas@gmail.com>
2021-01-02 19:10:51 +0100

    Create Pause menu in sample scene

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUi;
+    public static bool GameIsPaused = false;
+    public string optionSceneName;
+    public string menuSceneName;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadOptionMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(optionSceneName);
+        Debug.Log("Loading option menu...");
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(menuSceneName);
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
