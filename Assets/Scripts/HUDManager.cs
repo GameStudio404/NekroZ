44d28de1e56598ee278d38bc708893aa38880195
Thomas Ducret <ducret.thomas@gmail.com>
2021-01-24 19:18:42 +0100

    Add Team display (visual)

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDManager : MonoBehaviour
+{
+    private List<GameObject> teamList;
+    public GameObject teamMate;
+    public Transform team;
+
+    void Start()
+    {
+        teamList = new List<GameObject>();
+        GameObject tmp = Instantiate(teamMate, teamMate.transform);
+        tmp.SetActive(true);
+        teamList.Add(tmp);
+
+        foreach (GameObject item in teamList)
+        {
+            item.transform.SetParent(team);
+        }
+    }
+    
+    public void AddMate()
+    {
+        Debug.Log("You have " + teamList.Count + " mates !");
+        if (teamList.Count < 7)
+        {
+            GameObject tmp = Instantiate(teamMate, teamMate.transform);
+            tmp.SetActive(true);
+            tmp.transform.SetParent(team);
+            teamList.Add(tmp);
+        } else
+        {
+            Debug.Log("You already have 7 mates !");
+        }
+    }
+}
